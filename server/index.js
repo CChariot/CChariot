c733d59923e906d41a14de6b703f9c43c547bae3
Lihan <lihanzhan@gmail.com>
2020-03-30 12:43:47 -0400

    added get and post callback functions

--- server/index.js
+++ server/index.js
@@ -8,9 +8,18 @@ const ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(express.json);
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use("/api/employees", require("./api/employees"));
+
+if (ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}!`);
